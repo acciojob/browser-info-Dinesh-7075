a812cdd8aa96f65f44e57628788c37f019d0d7e8
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-22 06:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function browerInfo(){
+    // console.log(navigator.userAgent);
+    //     document.querySelector("#browser-info").innerText= `You are using ${navigator.userAgent}  version` ;
+
+        var result = bowser.getParser(window.navigator.userAgent);
+        console.log(result);
+      document.write("You are using " + result.parsedResult.browser.name +
+               " version " + result.parsedResult.browser.version);
+}
+browerInfo();
